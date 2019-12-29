cd0f16e725dd1f35657d4f8dddd241b2b7d2532d
Oyestimmy <oyestimmy@gmail.com>
2019-12-29 01:37:14 -0800

    Updates

---
+++ precache-manifest.cd20d0c10f0ecc49814e18e4599ad427.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06defb07d1eec1d7ca10565d80b7b5d",
+    "url": "/Oyestimmy/project-app.git/index.html"
+  },
+  {
+    "revision": "8310614e14d9c895d43b",
+    "url": "/Oyestimmy/project-app.git/static/css/main.3c9f183c.chunk.css"
+  },
+  {
+    "revision": "3e5184f47d5e14aedf52",
+    "url": "/Oyestimmy/project-app.git/static/js/2.22ebd21d.chunk.js"
+  },
+  {
+    "revision": "8310614e14d9c895d43b",
+    "url": "/Oyestimmy/project-app.git/static/js/main.a5ed4e0d.chunk.js"
+  },
+  {
+    "revision": "d1a6ca5baac8f98747e4",
+    "url": "/Oyestimmy/project-app.git/static/js/runtime~main.8f427a3e.js"
+  }
+]);
